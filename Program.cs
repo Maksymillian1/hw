ebce694fc84368af69adfceb4f82f88bf4aeb4ec
Maksymillian <jessevananholt24@gmail.com>
2022-11-29 18:56:54 +0300

    Zadacha 1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 1
+
+Console.WriteLine("введите число");
+   while(true)
+   {
+    stringinput = Console.ReadLine();
+    if(!input.Equals("exit"))
+     Console.WriteLine("{0} ->", input [1]);
+    else
+        break;
+   }
